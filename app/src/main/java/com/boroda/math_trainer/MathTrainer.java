b77cb3d73c34fe2530a3e5bcb7f40bd96f1e4e81
mbor99 <mbor99@gmail.com>
2014-11-27 16:20:51 +0200

    added task class

--- app/src/main/java/com/boroda/math_trainer/MathTrainer.java
+++ app/src/main/java/com/boroda/math_trainer/MathTrainer.java
@@ -1,6 +1,10 @@
 package com.boroda.math_trainer;
 
 public class MathTrainer {
+    public Task generateTask(){
+
+        return new Task();
+    }
     public void sum(){
 
     }
---
+++ app/src/main/java/com/boroda/math_trainer/Task.java
@@ -0,0 +1,14 @@
+package com.boroda.math_trainer;
+
+public class Task {
+    int first=0;
+    int second=0;
+    enum OPERATIONS {SUM, MINUS,MULTIPLE, DIVIDE }
+
+    public Task() {
+        Random random = new Random();
+        this.first = random.;
+        this.second = random;
+        OPERATIONS operation=OPERATIONS.get(RANDOM.nextInt(OPERATIONS.size);
+    }
+}
