ef628e2b0d509a0b102f42d3f0a7e33ee7cfd835
mbor99 <mbor99@gmail.com>
2014-11-27 16:04:39 +0200

    added main math class

---
+++ app/src/main/java/com/boroda/math_trainer/MathTrainer.java
@@ -0,0 +1,21 @@
+package com.boroda.math_trainer;
+
+public class MathTrainer {
+    public void sum(){
+
+    }
+    public void minus(){
+
+    }
+    public void multiple(){
+
+    }
+    public void divade(){
+
+    }
+    public boolean calculate(){
+
+        return false;
+    }
+}
+
