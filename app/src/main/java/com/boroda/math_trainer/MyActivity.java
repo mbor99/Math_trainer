2dd2560b330700f44560d9014ef665c21e86dc01
mbor99 <mbor99@gmail.com>
2014-12-02 22:49:58 +0200

    added init buttons

--- app/src/main/java/com/boroda/math_trainer/MyActivity.java
+++ app/src/main/java/com/boroda/math_trainer/MyActivity.java
@@ -4,15 +4,65 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
 
 
 public class MyActivity extends Activity {
+
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
+    Button button0;
+    Button buttonClear;
+    Button buttonCheck;
+    Button buttonNextTask;
+    TextView resultView;
+    TextView taskView;
+    TextView inputView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        MathTrainer mathTrainer= new MathTrainer();
+        MathTrainer mathTrainer = new MathTrainer();
+
+        initialyzeElements();
+
+
+
+    }
+
+
+    private void initialyzeElements(){
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        button0 = (Button) findViewById(R.id.button0);
+        buttonCheck = (Button) findViewById(R.id.buttonCheck);
+        buttonClear = (Button) findViewById(R.id.buttonClear);
+        buttonNextTask = (Button) findViewById(R.id.buttonNextTask);
+
+        resultView = (TextView) findViewById(R.id.resultView);
+        taskView = (TextView) findViewById(R.id.taskView);
+        inputView = (TextView) findViewById(R.id.inputField);
     }
 
 
